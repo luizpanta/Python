bb7b64ff57f2646df4103942cc2f94d82013eacc
luizpanta <luizpantarotti@gmail.com>
2023-06-29 06:55:41 -0300

    Atualização

--- teste.py
+++ teste.py
@@ -21,22 +21,31 @@ while (not terminou):
 print ("P = ", p)
 print ("I = ", i)
 '''
-def maior_primo(numero):
-    def eh_primo(numero):
-        if numero < 2:
-            return False
-        for i in range(2, int(numero ** 0.5) + 1):
-            if numero % i == 0:
-                return False
-        return True
+# def maior_primo(numero):
+#     def eh_primo(numero):
+#         if numero < 2:
+#             return False
+#         for i in range(2, int(numero ** 0.5) + 1):
+#             if numero % i == 0:
+#                 return False
+#         return True
 
-    while numero >= 2:
-        if eh_primo(numero):
-            return numero
-        numero -= 1
+#     while numero >= 2:
+#         if eh_primo(numero):
+#             return numero
+#         numero -= 1
 
-    return None
+#     return None
 
 
-num = int(input("DIgite: "))
-maior_primo(num)
+# num = int(input("DIgite: "))
+# maior_primo(num)
+
+i = 0
+while i < 3:
+  j = 0
+  while j < 3:
+    print(3*i+j+1
+          )
+    j = j + 1
+  i = i + 1
